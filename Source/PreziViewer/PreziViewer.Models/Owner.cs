55fbad909ae2809790ea07e934ca8f7847ed64f5
eomar <25548321+Eomar21@users.noreply.github.com>
2025-02-18 15:31:29 +0100

    Created base models

---
+++ Source/PreziViewer/PreziViewer.Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace PreziViewer.Models
+{
+    public class Owner
+    {
+        public Guid Id { get; }
+        public string FirstName { get; }
+        public Uri LastName { get; }
+    }
+}
---
+++ Source/PreziViewer/PreziViewer.Models/Presentation.cs
@@ -0,0 +1,14 @@
+﻿namespace PreziViewer.Models
+{
+    public class Presentation
+    {
+        public Guid Id { get; }
+        public string Title { get; }
+        public Uri ThumbnailUrl { get; }
+        public string Description { get; }
+        public string Privacy { get; } // TODO  - change to enum
+        public DateTime LastModified { get; }
+        public Owner Owner { get; }
+
+    }
+}
