bd40db58527f1100e31331dd76c2f797e3a8d043
eomar <25548321+Eomar21@users.noreply.github.com>
2025-02-20 22:45:32 +0100

    DetailedPresentationView populated wip

--- Source/PreziViewer/PreziViewerApp/ViewModels/DetailedPresentationViewModel.cs
+++ Source/PreziViewer/PreziViewerApp/ViewModels/DetailedPresentationViewModel.cs
@@ -24,7 +24,10 @@ namespace PreziViewer.App.ViewModels
 
         public string Title => m_Presentation.Title;
         public string Description => m_Presentation.Description;
-
+        public string Privacy => m_Presentation.Privacy;
+        public string LastModifiedDate => m_Presentation.LastModified.ToLongDateString();
+        public string LastModifiedTime => m_Presentation.LastModified.ToLongTimeString();
+        public string Owner => string.Concat(m_Presentation.Owner.FirstName, " ", m_Presentation.Owner.LastName);
         public string? UrlPathSegment => m_Presentation.Id.ToString();
 
     }
