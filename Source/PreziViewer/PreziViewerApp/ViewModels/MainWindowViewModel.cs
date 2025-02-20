52ded8e4249a0e580e4638cda5de90f96a63b883
eomar <25548321+Eomar21@users.noreply.github.com>
2025-02-21 00:27:07 +0100

    Dispose commands and subscription

--- Source/PreziViewer/PreziViewerApp/ViewModels/MainWindowViewModel.cs
+++ Source/PreziViewer/PreziViewerApp/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,17 @@
 ﻿using PreziViewer.Services.Interface;
 using ReactiveUI;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 
 namespace PreziViewer.App.ViewModels
 {
-    public class MainWindowViewModel : ViewModelBase, IScreen
+    public class MainWindowViewModel : ViewModelBase, IScreen, IDisposable
     {
         public RoutingState Router { get; }
         public readonly IPresentationFetcher m_PresentationFetcher;
         private string m_StatusText = "Offline";
+        private CompositeDisposable m_Disposable = new();
+
         public string StatusText
         {
             get => m_StatusText;
@@ -26,12 +29,17 @@ namespace PreziViewer.App.ViewModels
             isSourceOnline.Subscribe(x =>
             {
                 StatusText = x ? "Online" : "Offline";
-            });
+            }).DisposeWith(m_Disposable);
         }
 
         public void NavigateToPresentations()
         {
             Router.Navigate.Execute(new PresentationsViewModel(m_PresentationFetcher, this));
+        }
+
+        public void Dispose()
+        {
+            m_Disposable.Dispose();
         }
     }
 }
--- Source/PreziViewer/PreziViewerApp/ViewModels/OnePresentationViewModel.cs
+++ Source/PreziViewer/PreziViewerApp/ViewModels/OnePresentationViewModel.cs
@@ -1,25 +1,32 @@
 ﻿using PreziViewer.Models;
 using ReactiveUI;
 using System.Reactive;
+using System.Reactive.Disposables;
 
 namespace PreziViewer.App.ViewModels
 {
-    public class OnePresentationViewModel : ViewModelBase
+    public class OnePresentationViewModel : ViewModelBase, IDisposable
     {
         private Presentation m_Presntation;
         public IScreen HostScreen;
+        private CompositeDisposable m_Disposables = new();
         public ReactiveCommand<Unit, Unit> GoToDetailedViewCommand { get; }
 
         public OnePresentationViewModel(Presentation presentation, IScreen hostScreen)
         {
             m_Presntation = presentation;
             HostScreen = hostScreen;
-            GoToDetailedViewCommand = ReactiveCommand.Create(GoToDetailedView);
+            GoToDetailedViewCommand = ReactiveCommand.Create(GoToDetailedView).DisposeWith(m_Disposables);
         }
 
         private void GoToDetailedView()
         {
             HostScreen.Router.Navigate.Execute(new DetailedPresentationViewModel(m_Presntation, HostScreen));
+        }
+
+        public void Dispose()
+        {
+            m_Disposables.Dispose();
         }
 
         public string Title => m_Presntation.Title;
